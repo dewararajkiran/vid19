6b749e6574dfe1763c4973a361a457d8f9a81f03
dewararajkiran <dewara.rajkiran@gmail.com>
2020-03-26 16:48:07 +0530

    vid19 initial commit

---
+++ config.js
@@ -0,0 +1,14 @@
+module.exports =  {
+    skillName : "Smart City",
+    devConsoleTesting : 'true', // This is for address as In dev console we are not getting location
+    mockService : 'false',
+    salesforceApiKey : 'a071d5e196f3c9ca5d83256c0a1e2952c3d6b6246fdf412f0b20ff4705c1d59e',
+    salesforceApiEndpoint : 'https://threeoneone-demo-citizen.herokuapp.com',
+    salesforceUrl : 'https://incapsulate-threeoneone-demo-dev-ed.my.salesforce.com',
+	salesforceInstanceUrl : 'https://incapsulate-threeoneone-demo-dev-ed.my.salesforce.com',
+    miscServiceTypeCode : 'ECCCITCO',
+    miscServiceTypeName : 'Miscellaneous',
+    messagePrefix : 'Sent from your Twilio trial account -',
+    welcomeQuestionId : "qe_language",
+	nextVersion : false
+}
